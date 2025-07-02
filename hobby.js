2d41a9210eb6e7131a30f2b35dbfd7dd865dd695
linyuzhen0108 <147609383+linyuzhen0108@users.noreply.github.com>
2025-07-02 23:06:27 +0800

    hobby.js

--- hobby.js
+++ hobby.js
@@ -4,14 +4,45 @@ const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 const navToggle = document.getElementById('nav-toggle');
 const navMenu = document.getElementById('nav-menu');
-
-let currentIndex = 0;
-const total = images.length;
+const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
 
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
     navToggle.classList.toggle('active');
+
+    // 如果是關閉狀態，就清除所有展開的子選單
+    if (!navMenu.classList.contains('active')) {
+        document.querySelectorAll('.nav-item.dropdown').forEach(item => {
+            item.classList.remove('open');
+        });
+    }
 });
+
+dropdownToggles.forEach(toggle => {
+    toggle.addEventListener('click', (e) => {
+        if (window.innerWidth <= 768) {
+            e.preventDefault(); // 阻止跳轉連結
+
+            const currentItem = toggle.parentElement;
+
+            const isOpen = currentItem.classList.contains('open');
+
+            // 先關掉全部
+            document.querySelectorAll('.nav-item.dropdown').forEach(item => {
+                item.classList.remove('open');
+            });
+
+            // 如果剛剛沒開，就開它
+            if (!isOpen) {
+                currentItem.classList.add('open');
+            }
+        }
+    });
+});
+
+let currentIndex = 0;
+const total = images.length;
+
 
 function updateSlide() {
     const firstImage = images[0];
